0e0545ae9916f9703564c39c9f5cabec1b209fa7
OkaRitsu <riku26clm@gmail.com>
2025-03-30 20:53:42 +0900

    出力方法を更新

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import argparse
+import json
 import os
+import time
 
 import whisper
 
@@ -14,11 +16,20 @@ args = parser.parse_args()
 
 # Load the model
 model = whisper.load_model("turbo")
+
 # Transcribe the audio file
-result = model.transcribe(args.audio)
+start = time.time()
+result = model.transcribe(args.audio, verbose=True)
+running_time = time.time() - start
+# Print the running time in HH:MM:SS format
+hours, remainder = divmod(running_time, 3600)
+minutes, seconds = divmod(remainder, 60)
+print(f"Transcription completed in {int(hours):02}:{int(minutes):02}:{int(seconds):02}")
 
 # Save the transcription to a file
 os.makedirs(args.output_dir, exist_ok=True)
 filename = os.path.splitext(os.path.basename(args.audio))[0]
 with open(os.path.join(args.output_dir, f"{filename}.txt"), "w") as f:
     f.write(result["text"])
+with open(os.path.join(args.output_dir, f"{filename}.json"), "w") as f:
+    json.dump(result["segments"], f, indent=4)
